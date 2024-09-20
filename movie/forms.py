e15169a2bf58db0aa326d0bc63807249d40d4594
AyambaSumaila <sumailaayamba15@gmail.com>
2024-09-20 06:23:51 +0000

    "Screenshots added"

--- movie/forms.py
+++ movie/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm, Textarea
 from .models import Review
 
+
+
 class ReviewForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(ModelForm, self).__init__(*args, **kwargs)
